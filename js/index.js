00b8428c78d89997b89b12665b8945cf5054108f
brit-starks <brittneytechiethomas@gmail.com>
2019-07-12 03:57:22 -0700

    Add mouseover & mouseleave Events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+const contentSec = document.querySelector('.content-section');
+
+contentSec.addEventListener('mouseover', event => contentSec.style.color = 'red');
+contentSec.addEventListener('mouseleave', event => contentSec.style.color = 'black');
