47c21249567520193b42a6e54ef1f4be78db31da
brit-starks <brittneytechiethomas@gmail.com>
2019-07-12 18:41:22 -0700

    Add double click event

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 // Your code goes here
 
+// 1. mouaeover 
 const contentSec = document.querySelector('.content-section');
 
 contentSec.addEventListener('mouseover', event => contentSec.style.color = 'red');
+
+// 2. mouseleave
 contentSec.addEventListener('mouseleave', event => contentSec.style.color = 'black');
+
+// 3. double click here
+const welcome = document.querySelector('h2');
+
+welcome.addEventListener('dblclick', e => 
+  welcome.style.color = 'orangered'
+);
+
+// 4. scroll
+
+// 5. keydown
+
